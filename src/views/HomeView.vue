3b13a327678212427878dfa209c7bf3525299e2a
Nerisa Afiani <nerisaafiani22@gmail.com>
2023-12-21 15:16:07 +0700

    1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <a href="/login">
+            <button>Login</button>
+        </a>
+        <a href="/channels">
+            <button>Channels</button>
+        </a>
+    </div>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const email = ref('');
+const password = ref('');
+
+const login = async () => {
+    try {
+        const res = await axios.post('http://localhost:3000/api/user/login', {
+            email: email.value,
+            password: password.value
+        }, {
+            withCredentials: true
+        })
+        console.log(res);
+        alert('Login success')
+    } catch (e) {
+        alert(e.message);
+    }
+}
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="login">
+            <input type="text" v-model="email">
+            <input type="password" v-model="password">
+            <button>Login</button>
+        </form>
+    </div>
+</template>
